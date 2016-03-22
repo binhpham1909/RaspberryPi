e256e95c23584700ec0f15c56d1fdfb45fcd0e0c
Pham Huu Binh <binhpham1909@gmail.com>
2016-03-22 11:18:02 +0700

    edit sim900a
    ad debug

--- python/sim900a.py
+++ python/sim900a.py
@@ -8,144 +8,152 @@ class SIM900A:
 	def __init__(self, port, baud):
 		self.port = port
 		self.baud = baud
+		self.DEBUG = 1;
 	def Connect(self):
 		self.ser = serial.Serial(self.port, self.baud, timeout=5)
 		time.sleep(1)
 	def Disconnect(self):
 		self.ser.close()
-# Module function
-	def checkModule(self):
-		respone = ''
+	def flushSerial(self):
 		self.ser.reset_input_buffer()
 		self.ser.reset_output_buffer()
-		self.ser.write('AT\r\n')
-		time.sleep(1)
+	def readSerial(self, timewait):
+		respone = ''
+		time.sleep(timewait)
 		while ser.inWaiting() > 0:
 			respone += self.ser.read(1)
+		return respone
+#debug 
+	def debug(self,message):
+		if(self.DEBUG):
+			print message
+			sys.stdout.flush()
+# Module function
+	def checkModule(self):
+		self.flushSerial()
+		self.ser.write('AT\r\n')
+		self.debug('Check module SIM900A.\r\n')
+		self.debug('>>: AT\r\n')
+		time.sleep(1)
+		respone = self.readSerial()
+		self.debug('<<: '+respone)
 		if('OK' in respone):
 			return 1
 		else:
 			return 0
 	def checkSIM(self):
-		respone = ''
-		self.ser.reset_input_buffer()
-		self.ser.reset_output_buffer()
-		self.ser.write('AT+CPIN?' + '\r\n');
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
+		self.flushSerial()
+		self.ser.write('AT+CPIN?\r\n');
+		self.debug('Check SIM/Card.\r\n')
+		self.debug('>>: AT+CPIN?\r\n')
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
 		if('READY' in respone):
 			return 1
 		else:
 			return 0
 	def resetDefaultConfig(self):
-		respone = ''
-		self.ser.reset_input_buffer()
-		self.ser.reset_output_buffer()
+		self.flushSerial()
 		self.ser.write('ATZ\r\n');
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
+		self.debug('Set SIM900A to default config.\r\n')
+		self.debug('>>: ATZ\r\n')
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
 			return 1
 		else:
 			return 0
 	def setCommandEcho(self, echoMode):
-		respone = ''
-		self.ser.reset_input_buffer()
-		self.ser.reset_output_buffer()
+		self.flushSerial()
+		self.debug('Set command echo.\r\n')
 		if(!echoMode):
 			self.ser.write('ATE0\r\n');
+			self.debug('Echo command OFF\r\n')
+			self.debug('>>: ATE0\r\n')
 		else:
 			self.ser.write('ATE1\r\n');
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
+			self.debug('Echo command ON\r\n')
+			self.debug('>>: ATE1\r\n')
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
 			return 1
 		else:
 			return 0	
 # function for call
-	def enableCallerInfo(self):
-		respone = ''
-		self.ser.reset_input_buffer()
-		self.ser.reset_output_buffer()
-		self.ser.write('AT+CLIP=1\r\n');
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
+	def enableCallerInfo(self, mode):
+		self.flushSerial()
+		self.debug('Enable/Disable caller info.\r\n')
+		if(!mode):
+			self.ser.write('AT+CLIP=0\r\n');
+			self.debug('>>: AT+CLIP=0\r\n')
+			self.debug('Disable caller info.\r\n')
+			
+		else:
+			self.ser.write('AT+CLIP=1\r\n');
+			self.debug('>>: AT+CLIP=1\r\n')
+			self.debug('Enable caller info.\r\n')
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
 			return 1
 		else:
 			return 0
 	def callNumber(self, phonenumber):
-		respone = ''
-		self.ser.reset_input_buffer()
-		self.ser.reset_output_buffer()
-		self.ser.write('ATD%s;\r\n'%(phonenumber));
-		time.sleep(2)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
-			if(respone.find('NO DIALTONE')>0):
+		self.flushSerial()
+		self.ser.write('ATD%s;\r\n'%(phonenumber))
+		self.debug('Make call to %s.\r\n'%(phonenumber))
+		self.debug('>>: ATD%s;\r\n'%(phonenumber))
+		respone = self.readSerial(2)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
+			if('NO DIALTONE' in respone):
 				return 'NO DIALTONE',''
-			elif(respone.find('BUSY')>0):
+			elif('BUSY' in respone):
 				return 'BUSY',''
-			elif(respone.find('NO CARRIER')>0):
+			elif('NO CARRIER' in respone):
 				return 'NO CARRIER',''
-			elif(respone.find('NO ANSWER')>0):
+			elif('NO ANSWER' in respone):
 				return 'NO ANSWER',''
-			elif(respone.find('CUSD')>0):
+			elif('CUSD' in respone):
 				return 'USSD',respone
-	def callUSSD(self, phonenumber):
-		dvrespone = ''
-		respone = ''
-		self.ser.write('ATD%s;\r\n'%(phonenumber));
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			dvrespone += self.ser.read(1)
-		if(dvrespone.find('OK')>0):
-			time.sleep(1)
-			while ser.inWaiting() < 0:
-			while ser.inWaiting() > 0:
-				respone += self.ser.read(1)			
-				elif(respone.find('CUSD')>0):
-				return 'USSD',respone
-	def callUSSD(self, phonenumber):
-		stt,res = self.callNumber('*' + phonenumber + '#')
+	def payAccount(self, code):
+		comm,res = self.callNumber('*100*' + code + '#')
 		return res
-	def callUSSD(self, phonenumber, code):
-		stt,res = self.callNumber('*' + phonenumber + '*' + code + '#')
+	def checkBalance(self):
+		comm,res = self.callNumber('*101#')
 		return res
 	def hangupCall(self):
-		respone = ''
+		self.flushSerial()
 		self.ser.write('ATH\r\n');
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
+		self.debug('Hangup call.\r\n')
+		self.debug('>>: ATH\r\n')
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
 			return 1
 		else:
 			return 0
 	def acceptCall(self):
-		respone = ''
+		self.flushSerial()
 		self.ser.write('ATA1\r\n');
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
+		self.debug('Accept call.\r\n')
+		self.debug('>>: ATA1\r\n')
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
 			return 1
 		else:
 			return 0
 
 #function for SMS
 	def setSMSTextMode(self, mode):
-		respone = ''
 		self.ser.write('AT+CMGF==%d\r\n'%(mode));
-		time.sleep(1)
-		while ser.inWaiting() > 0:
-			respone += self.ser.read(1)
-		if(respone.find('OK')>0):
+		self.debug('Set SMS to Text mode.\r\n')
+		self.debug('>>: AT+CMGF==%d\r\n'%(mode))
+		respone = self.readSerial(1)
+		self.debug('<<: '+respone)
+		if('OK' in respone):
 			return 1
 		else:
 			return 0		
